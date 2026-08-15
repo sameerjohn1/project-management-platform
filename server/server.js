0e7f93e47ebc22784ab97eabd01591777c90067c
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-08-15 16:54:10 +0500

    Configure Vercel deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
+const isVercel = Boolean(process.env.VERCEL);
 
 app.use(express.json());
 app.use(cors());
@@ -19,6 +20,10 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (!isVercel) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
